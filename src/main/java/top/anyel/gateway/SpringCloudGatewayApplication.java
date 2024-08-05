a6867e132097203f56291dc65f1141e9b0beb989
anyel-ec <paulangel826@gmail.com>
2024-08-05 10:29:06 -0500

    add cors permit

--- src/main/java/top/anyel/gateway/SpringCloudGatewayApplication.java
+++ src/main/java/top/anyel/gateway/SpringCloudGatewayApplication.java
@@ -2,6 +2,7 @@ package top.anyel.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
